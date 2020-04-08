7560dc01f7aa3dfb9a9173d13341b5b6bc85dfe8
Miroshnichenko Vladislav <vm@sourceangel.dk>
2020-04-08 21:26:07 +0300

    Updates

---
+++ precache-manifest.ba0071658845840f65950ba4ddfc14fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a7ff6eac2ec40594dc1e918487fc71",
+    "url": "/header-test-task/index.html"
+  },
+  {
+    "revision": "2c72782ac0ba46b58c65",
+    "url": "/header-test-task/static/css/main.2779671f.chunk.css"
+  },
+  {
+    "revision": "40cb129cf6b8936ffe77",
+    "url": "/header-test-task/static/js/2.3ff4a817.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/header-test-task/static/js/2.3ff4a817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c72782ac0ba46b58c65",
+    "url": "/header-test-task/static/js/main.bd07d6af.chunk.js"
+  },
+  {
+    "revision": "2dfbce940edcff6f83cb",
+    "url": "/header-test-task/static/js/runtime-main.9919c914.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/header-test-task/static/media/logo.5d5d9eef.svg"
+  }
+]);
